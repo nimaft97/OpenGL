27d35bd70aaafc6e830d1e5bec5c2bce1410d804
nimaft97 <nimafatholahi@gmail.com>
2023-01-03 18:13:28 -0800

    committing small changes to keep everything updated

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -39,6 +39,10 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    //enable vertex attribute pointer
+    glEnableVertexAttribArray(0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
