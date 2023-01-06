67e6e4c61704d9bbb724470fdcc06f0eda95aedd
nimaft97 <nimafatholahi@gmail.com>
2023-01-05 18:32:05 -0800

    committing everything

--- src/Application.cpp
+++ src/Application.cpp
@@ -108,6 +108,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
     if (!window)
     {
         glfwTerminate();
@@ -116,6 +117,8 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    // to make changes smoother
+    glfwSwapInterval(1);
     
     if (glewInit() != GLEW_OK) {
         std::cout << "GLEW couldn't start!\n";
@@ -140,30 +143,43 @@ int main(void)
 
     // vertex buffer
     unsigned int buffer;
-    GLCall(glGenBuffers(1, &buffer))
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer))
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW))
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0))
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
     //enable vertex attribute pointer
-    GLCall(glEnableVertexAttribArray(0))
+    GLCall(glEnableVertexAttribArray(0));
 
     // index buffer
     unsigned int ibuffer;
-    GLCall(glGenBuffers(1, &ibuffer))
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer))
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW))
+    GLCall(glGenBuffers(1, &ibuffer));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
     // create shaders
     ShaderProgramSource source(ParseShader("res/shaders/Basic.shader"));
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    GLCall(glUseProgram(shader))
+    GLCall(glUseProgram(shader));
+
+    GLCall(int location = glGetUniformLocation(shader, "u_Color")); 
+
+    float r = 0.0f, increment = 0.05f;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT))
+
+        // set color in runtime
+        GLCall(glUniform4f(location, r, 0.2f, 0.8f, 1.0f));
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        r += increment;
 
         // draw a simple triangle
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr))
